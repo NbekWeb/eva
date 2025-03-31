c5df7db9b1709c77f2553c311ba1270e8084a9fa
NbekWeb <nbekdev@gmail.com>
2025-04-01 00:53:22 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="px-10">
+    <p class="bg-blue-500 pb-0 text-lg">sa</p>
+    <span class="bg-red-500">sa</span>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
