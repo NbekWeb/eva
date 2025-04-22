16d8d8e58c821fd4aa377927d04fb987cf4661d3
NbekWeb <nbekdev@gmail.com>
2025-04-22 12:01:59 +0500

    sa

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -12,7 +12,6 @@ export default defineNuxtRouteMiddleware((to) => {
     }
 
     if (!accessToken && to.path !== "/auth/vk/login/callback/") {
-        console.log(to.path,to.path !== "/auth/vk/login/callback/",'salom')
-        // return navigateTo("/login");
+        return navigateTo("/login");
     }
 });
