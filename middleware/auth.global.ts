39421ca3cf5504a04d0899740876bfaa924ca4fa
NbekWeb <nbekdev@gmail.com>
2025-04-22 11:39:42 +0500

    sa

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,17 +1,18 @@
 export default defineNuxtRouteMiddleware((to) => {
     if (!process.client) return;
-
+  
     const accessToken = localStorage.getItem("access_token");
-    const publicPaths = ["/login", "/regis",'/auth/vk/login/callback'];
-
+  
+    const publicPaths = ["/login", "/regis", "/auth/vk/login/callback"];
+  
+    // Allow access to public pages without token
     if (publicPaths.includes(to.path)) {
-        if (accessToken) {
-            return navigateTo("/");
-        }
-        return;
+      return;
     }
-
+  
+    // If no access token and not a public page, redirect to login
     if (!accessToken) {
-        return navigateTo("/login");
+      return navigateTo("/login");
     }
-});
+  });
+  
