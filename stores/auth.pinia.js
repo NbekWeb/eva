9638066f1b49245b2618d24b6d699e42036d7e03
NbekWeb <nbekdev@gmail.com>
2025-04-22 11:35:36 +0500

    sa

--- stores/auth.pinia.js
+++ stores/auth.pinia.js
@@ -96,6 +96,43 @@ const useAuth = defineStore("auth", {
           core.loadingUrl.delete("account/auth/check/vk/");
         });
     },
+    postGoogle(data, callback, erorCallback) {
+      const core = useCore();
+      core.loadingUrl.add("account/auth/vk/");
+      api({
+        url: "accounts/auth/vk/",
+        method: "POST",
+        data,
+      })
+        .then(({ data }) => {
+          localStorage.setItem("access_token", data.access_token);
+          callback();
+        })
+        .catch((error) => {
+          erorCallback();
+          message.error("Что-то пошло не так!");
+        })
+        .finally(() => {
+          core.loadingUrl.delete("account/auth/check/vk/");
+        });
+    },
+    getGoogle( callback) {
+      const core = useCore();
+      core.loadingUrl.add("google/");
+      api({
+        url: "accounts/auth/google/",
+        method: "GET",
+       
+      })
+        .then(({ data }) => {
+          callback(data);
+        })
+        .catch((error) => {
+        })
+        .finally(() => {
+          core.loadingUrl.delete("google");
+        });
+    },
   },
 });
 
