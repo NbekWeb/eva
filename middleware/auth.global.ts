ecf1551e5753ec10c304c525200c97ca5956c9f6
NbekWeb <nbekdev@gmail.com>
2025-04-23 20:26:06 +0500

    sa

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,17 +1,26 @@
 export default defineNuxtRouteMiddleware((to) => {
-    if (!process.client) return;
+  if (!process.client) return;
 
-    const accessToken = localStorage.getItem("access_token");
-    const publicPaths = ["/login", "/regis",'/auth/vk/login/callback','/api/auth/google/callback'];
+  const accessToken = localStorage.getItem("access_token");
+  const publicPaths = [
+    "/login",
+    "/regis",
+    "/auth/vk/login/callback",
+    "/api/auth/google/callback",
+  ];
 
-    if (publicPaths.includes(to.path)) {
-        if (accessToken) {
-            return navigateTo("/");
-        }
-        return;
+  if (publicPaths.includes(to.path)) {
+    if (accessToken) {
+      return navigateTo("/");
     }
+    return;
+  }
 
-    if (!accessToken && to.path !== "/auth/vk/login/callback/") {
-        return navigateTo("/login");
-    }
+  if (
+    !accessToken &&
+    to.path !== "/auth/vk/login/callback/" &&
+    to.path !== "/api/auth/google/callback/"
+  ) {
+    return navigateTo("/login");
+  }
 });
